05a45d71f9c0df0f9c51d0f78e8a89187f244b73
tepeko98 <klghyu2356@gmail.com>
2023-09-25 17:10:44 +0900

    q09.25

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,31 @@
-alert("hi");
+// alert("hi");
+
+// function, addeventlister 등 활용 가능
+// 이곳 frontend에서 backend와 연결해달라고 코드 작성
+
+// // 이 경우 protocol이 ws이 아닌 http이므로 작동하지 않는다.
+// const socket = new WebSocket("http://localhost:3000");
+
+// window.location 활용
+// 여기 있는 socket은 서버로의 연결이다.
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+// 서버가 open 될 경우 메시지 출력
+socket.addEventListener("open", () => {
+    console.log("Conneted to Server ✅");
+});
+
+// message 받을 때마다 내용 출력
+socket.addEventListener("message", (message) => {
+    console.log("New message: ", message.data);
+});
+
+// 서버가 close 될 경우 메시지 출력
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server ❎");
+});
+
+// 즉시 실행되지 않도록 기다리게 하기
+setTimeout(() => {
+    socket.send("hello from the browser!");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -14,6 +14,28 @@ app.get("/*", (_, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
+// web socket server 생성
 const wss = new WebSocket.Server({server});
+
+// socket은 브라우저와 유저 사이의 연결이므로 따로 저장해놔야 한다.
+// function handleConnection(socket) {
+//     // 여기 있는 socket(연결된 브라우저)이 실시간으로 frontend와 소통할 수 있다.
+//     console.log(socket);
+// };
+// websocket에서는 .on만으로 event:connection, function 받을 수 있다.
+// 새로운 연결을 기다리고, 작동할 함수 작성
+// wss.on("connection", handleConnection);
+// 최적화, connection이 생겼을 때 socket으로 즉시 메시지(socket.send("hello!")) 보낸다.
+wss.on("connection", (socket) => {
+    // socket에 있는 메서드 send 활용, 메시지 전송
+    // frontend에서 메시지 받기 event도 작성해야 한다.
+    console.log("Conneted to Browser ✅");
+    // socket에서 event listen하기
+    socket.on("close", () => {console.log("Disconnected from Browser ❎")});
+    socket.on("message", (message) => {
+        console.log(message.toString('utf8'));
+    });
+    socket.send("hello!");
+});
 
 server.listen(3000, handleListen);
